f8d7315dd661568e2ac26183392878fe1d00ff02
Duc Minh Vu <vu.minh.duc@framgia.com>
2016-06-13 11:12:52 +0700

    Add user login facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "tours#index"
 
+  devise_for :users, controllers: {omniauth_callbacks: "users/omniauth_callbacks"}
   resources :tours
 end
